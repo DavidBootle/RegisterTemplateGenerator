926e3afc43cde64dc9819e1aa8774a1da8a64280
David Bootle <davidtbootle@gmail.com>
2024-04-29 18:11:33 -0400

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import { useState } from 'react';
 import './App.css';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Register from './components/Register';
 
 function App() {
